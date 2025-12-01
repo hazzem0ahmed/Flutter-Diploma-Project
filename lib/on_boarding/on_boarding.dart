8342015d5b214fadbd1b5a821383f80d80729e39
Fc <waledmohand0@gmail.com>
2025-12-01 09:07:39 +0300

    make Pageview for all onboarding & add localizations

--- lib/on_boarding/on_boarding.dart
+++ lib/on_boarding/on_boarding.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:movies/core/app_asset.dart';
 import 'package:movies/core/app_colors.dart';
-import 'package:movies/on_boarding/on_boarding1.dart';
 
 import '../l10n/generated/app_localizations.dart';
 import '../presentation/widgets/elevated_button/elevated_button.dart';
-
+import 'on_boarding_final.dart';
 
 class OnBoarding extends StatelessWidget {
   static const String routeName = "/onboarding";
@@ -64,7 +63,7 @@ class OnBoarding extends StatelessWidget {
                 ),
                 SizedBox(height: 16),
                 ElevatedButtonWidget(onPressed: (){
-                  Navigator.pushReplacementNamed(context, OnBoarding1.routeName);
+                  Navigator.pushReplacementNamed(context, OnBoardingFinal.routeName);
                 },
                   text: 'Explore Now',)
               ],
