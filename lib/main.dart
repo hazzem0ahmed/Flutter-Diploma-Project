2d47a38300092df19525af09d9e1a09f55836e40
Fc <waledmohand0@gmail.com>
2025-12-01 09:07:39 +0300

    make Pageview for all onboarding & add localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movies/on_boarding/on_boarding3.dart';
 import 'package:movies/presentation/widgets/language/language_contract.dart';
 import 'package:movies/presentation/widgets/language/language_vm.dart';
 import 'l10n/generated/app_localizations.dart';
 import 'on_boarding/on_boarding.dart';
-import 'on_boarding/on_boarding1.dart';
-import 'on_boarding/on_boarding2.dart';
-import 'on_boarding/on_boarding4.dart';
-import 'on_boarding/on_boarding5.dart';
+import 'on_boarding/on_boarding_final.dart';
 
 void main() {
   runApp(
@@ -33,11 +29,8 @@ class MoviesApp extends StatelessWidget {
           supportedLocales: AppLocalizations.supportedLocales,
 routes: {
   OnBoarding.routeName :( _)=>OnBoarding(),
-  OnBoarding1.routeName :( _)=>OnBoarding1(),
-  OnBoarding2.routeName :( _)=>OnBoarding2(),
-  OnBoarding3.routeName :( _)=>OnBoarding3(),
-  OnBoarding4.routeName :( _)=>OnBoarding4(),
-  OnBoarding5.routeName :( _)=>OnBoarding5(),
+  OnBoardingFinal.routeName :( _)=>OnBoardingFinal(),
+
 },
  initialRoute: OnBoarding.routeName,
 
