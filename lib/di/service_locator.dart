6b13d29d4aaee2a7a064f963fb1a11781213e772
Mohamed Bayoumi <mohamedbayoumh194@gmail.com>
2025-12-04 23:56:15 +0200

    update the register screens

--- lib/di/service_locator.dart
+++ lib/di/service_locator.dart
@@ -1,16 +1,24 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import '../auth/data/datasource/contract/auth_remote_datasource.dart';
+import '../auth/data/datasource/impl/auth_remote_datasource_impl.dart';
+import '../auth/data/repo/auth_repo_impl.dart';
+import '../auth/domain/repository/auth_repository.dart';
+import '../auth/domain/use_case/signup_user_use_case.dart';
+import '../network/api_client.dart';
+import '../presentation/Register/resgister_cubit.dart';
 
 
 GetIt getIt = GetIt.instance;
 
-// void configureDependenciesGetIt() {
-//   // getIt.registerSingleton<Dio>(provideDio());
-//   getIt.registerSingleton<ApiClient>(ApiClient(getIt()));
-//   getIt.registerFactory<AuthRemoteDatasource>(
-//     () => AuthRemoteDatasourceImpl(getIt()),
-//   );
-//   getIt.registerFactory<AuthRepo>(() => AuthRepoImpl(getIt()));
-//   getIt.registerFactory<SignupUserUseCase>(() => SignupUserUseCase(getIt()));
-//   getIt.registerFactory<RegisterCubit>(() => RegisterCubit(getIt()));
-// }
+void configureDependenciesGetIt() {
+  // getIt.registerSingleton<Dio>(provideDio());
+  getIt.registerSingleton<ApiClient>(ApiClient(getIt()));
+  getIt.registerFactory<AuthRemoteDatasource>(
+    () => AuthRemoteDatasourceImpl(getIt()),
+  );
+  getIt.registerFactory<AuthRepo>(() =>
+      AuthRepoImpl(getIt(), getIt()));
+  getIt.registerFactory<SignupUserUseCase>(() => SignupUserUseCase(getIt()));
+  getIt.registerFactory<RegisterCubit>(() => RegisterCubit(getIt()));
+}
