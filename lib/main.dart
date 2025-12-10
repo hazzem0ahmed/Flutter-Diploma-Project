1a26de7cda518636ffe38c177225a0645c612bdf
Fc <waledmohand0@gmail.com>
2025-12-10 03:00:57 +0300

    add api suggestion screen & onboarding1

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,11 @@ import 'package:movies/presentation/screens/home_screen.dart';
 import 'package:movies/presentation/screens/splash_screen.dart';
 import 'package:movies/presentation/widgets/language/language_contract.dart';
 import 'package:movies/presentation/widgets/language/language_vm.dart';
-import 'auth/data/datasource/contract/auth_local_datasource.dart';
 import 'di/modules/service_locator.dart';
 import 'features/login/presentation/pages/login_screen.dart';
 import 'firebase_options.dart';
 import 'l10n/generated/app_localizations.dart';
-import 'on_boarding/on_boarding_final.dart';
+import 'on_boarding/on_boarding.dart';
 import 'on_boarding/on_boarding_final.dart';
 
 const String webClientId = 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com';
@@ -24,10 +23,6 @@ void main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   configureDependenciesGetIt();
-
-  final authLocal = getIt<AuthLocalDatasource>();
-  final String? savedToken = await authLocal.getSavedToken();
-
   runApp(
     BlocProvider<LanguageViewModel>(
       create: (context) => LanguageViewModel(),
@@ -50,17 +45,16 @@ class MoviesApp extends StatelessWidget {
           supportedLocales: AppLocalizations.supportedLocales,
 
           routes: {
+            OnBoardingScreen.routeName: (_) => OnBoardingScreen(),
             OnBoardingFinal.routeName: (_) => OnBoardingFinal(),
             LoginScreen.routeName: (context) => const LoginScreen(),
             RegisterScreen.routeName: (context) => const RegisterScreen(),
             SplashScreen.routeName: (context) => const SplashScreen(),
             HomeScreen.routeName: (context) => HomeScreen(),
             DetailsScreen.routeName: (context) => const DetailsScreen(),
-            OnBoardingFinal.routeName: (_) => OnBoardingFinal(),
-            LoginScreen.routeName: (context) => LoginScreen(),
-            RegisterScreen.routeName: (context) => RegisterScreen(),
+
           },
-          initialRoute: HomeScreen.routeName,
+          initialRoute: OnBoardingScreen.routeName,
         );
       },
     );
